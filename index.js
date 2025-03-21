a881e882205839e0e51354cbd3d1dc0424d727b7
Kingbanda <bandadesignske@gmail.com>
2025-03-21 08:45:22 +0300

    Arythmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 24;  
+const num4 = 5;  
+let multiply = num1 * num2; // 31 * 2 = 62
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+
+let mod = num3 % num4;
+
+const max = Math.max(5, 9, 12,17, 20);
